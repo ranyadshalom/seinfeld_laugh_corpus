9926f94395f022cfd18dcb7d34df09e9233ac70e
ranyadshalom <ranyadshalom@gmail.com>
2018-07-25 13:27:20 +0300

    Improved extract_laughter_times

--- corpus_creation/laugh_extraction/extract_laughter_times.py
+++ corpus_creation/laugh_extraction/extract_laughter_times.py
@@ -1,19 +1,20 @@
 import argparse
-from numpy import mean, array_split, array, median, percentile, std
-from math import sqrt
+from numpy import mean, array_split, array, median, percentile, std, sqrt
 from scipy.io.wavfile import read
-from collections import Counter
+from collections import Counter, namedtuple
 
 from corpus_creation.utils.utils import log10wrapper
 
+Laugh = namedtuple('Laugh', ['time', 'vol'])
 db_measurement_chunks_per_second = 10           # chunks (to measure dB of) per second.
 minimum_laughs = 85                             # if extracted less than this, something is wrong.
 
 
 def run(input, output):
     dbs = get_audio_dbs(input)
-    laughter_times = get_laughter_times(dbs)
+    laughters = get_laughters(dbs)
 
+    laughter_times = [l.time for l in laughters]
     verify_result(laughter_times)
     write_to_file(laughter_times, output)
 
@@ -29,43 +30,42 @@ def get_audio_dbs(audio_file):
     numchunks = int((len(wavdata) / samples_per_second) * db_measurement_chunks_per_second)
     chunks = array_split(wavdata, numchunks)
     chunks = [array(chunk, dtype='int64') for chunk in chunks]      # to prevent integer overflow
-    dbs = [20*log10wrapper( sqrt(mean(chunk**2)) ) for chunk in chunks]     # list of dB values for the chunks
+    dbs = [20*log10wrapper( mean(sqrt((chunk / 32767)**2)) ) for chunk in chunks]     # list of dB values for the chunks
 
     return dbs
 
 
-def get_laughter_times(dbs):
+def get_laughters(dbs):
     """
     :return: an array of the laugh timestamps in seconds.
     """
-    silence_threshold = mean(dbs) - std(dbs)
+    silence_threshold = mean(dbs)
 
-    laughter_times = []
+    laughters = []
     for i in range(len(dbs)):
-        s, fullness, m, result, mm = is_a_peak(dbs, i, silence_threshold)
+        s, fullness, m, result = is_a_peak(dbs, i, silence_threshold)
         if result:
             total_seconds = i/db_measurement_chunks_per_second
             # minutes, seconds = int(total_seconds / 60), total_seconds % 60
-            # print("%02d:%.1f LOL (std:%.3f, peak:%.3f, mean volume: %.3f, mean_without_peak: %.3f)" % (minutes, seconds, s, fullness, m, mm))
-            laughter_times.append(total_seconds)
-    return laughter_times
+            # print("%02d:%.1f LOL (std:%.3f, peak:%.3f, mean volume: %.3f" % (minutes, seconds, s, fullness, m))
+            laughters.append(Laugh(time=total_seconds, vol=m))
+    return laughters
 
 
 def is_a_peak(dbs, i, silence_threshold):
-    rng = 3 * db_measurement_chunks_per_second
+    detection_rng = 3 * db_measurement_chunks_per_second
+    vol_measurement_rng = int(1 * db_measurement_chunks_per_second)
 
     try:
-        if all(dB <= dbs[i] for dB in dbs[i:i+rng]) and all(dbs[i] >= dB for dB in dbs[i-rng:i]):
+        if all(dB <= dbs[i] for dB in dbs[i:i+detection_rng]) and all(dbs[i] >= dB for dB in dbs[i-detection_rng:i]):
             # is a peak
             if mean(dbs[i-rng:i+rng]) > silence_threshold:
-                std_of_interval = std(dbs[i-rng:i+rng])
-                mean_of_interval = mean(dbs[i-rng:i+rng])
-                if std_of_interval < mean_of_interval:
-                    # usually when the standard deviation of the interval is bigger than the mean, it is a FP
-                    return std_of_interval, dbs[i], mean_of_interval, True, mean(dbs[i-rng:i] + dbs[i+1:i + rng])
+                std_of_interval = std(dbs[i-vol_measurement_rng:i+vol_measurement_rng])
+                mean_of_interval = mean(dbs[i-vol_measurement_rng:i+vol_measurement_rng])
+                return std_of_interval, dbs[i], mean_of_interval, True
     except IndexError:
-        return None, None, None, None, None
-    return None, None, None, None, None
+        return None, None, None, None
+    return None, None, None, None
 
 
 def verify_result(laughter_times):
